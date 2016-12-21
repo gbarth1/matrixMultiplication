7fa850778fcf11a3d3483af1948acb793d8e3048
gbarth1 <grace.m.barth@gmail.com>
2016-12-21 14:29:25 -0500

    Initial commit

---
+++ src/com/company/MatrixMult.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/**
+ * Created by gb647 on 12/21/16.
+ */
+public class MatrixMult
+{
+    public static mult()
+    {
+        //pg 194 & pg 504
+        //return an array that is the product matrix of a and b
+        int product[][]= new int[][]{};
+        for(int cnt=1; cnt<4; cnt++)
+        {
+            //product=a[cnt][cnt]
+        }
+    }
+}
